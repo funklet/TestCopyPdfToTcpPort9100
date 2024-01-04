546dc44c051b0de4d4cb9e8af641e066aabf6991
Spencer Thompson <Spencer.Thompson@networklimited.co.uk>
2024-01-04 19:05:42 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Sockets;
+var printerIP = "10.6.51.27";
+var printerPort = 9100;
+var baseDirectory = Environment.CurrentDirectory;
+var fileName = "OriginalPdf.pdf";
+var fullFileName = Path.Combine(baseDirectory, fileName);
+using var fileStream = File.OpenRead(fullFileName);
+using (var tcpClient = new TcpClient())
+{
+    await tcpClient.ConnectAsync(printerIP, printerPort);
+    if (tcpClient.Connected)
+    {
+        using var printerStream = tcpClient.GetStream();
+        byte[] buffer = new byte[tcpClient.SendBufferSize];
+        await fileStream.CopyToAsync(printerStream, tcpClient.SendBufferSize);
+        await fileStream.FlushAsync();
+        await printerStream.FlushAsync();
+        await printerStream.DisposeAsync();
+        await fileStream.DisposeAsync();
+    }
+}
